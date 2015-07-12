284990e30084c7dc9d0d5eac82a3dfd04a0c5b35
Chetan Arya <code@chetanarya.com>
2015-07-12 17:20:16 +0530

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularEmptyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularEmptyApp
+ */
+angular.module('angularEmptyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
